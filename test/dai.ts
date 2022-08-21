d1d8a376a5fe95d3ddf15f07c94350f23ead7db7
Evolve <Artevolve@yahoo.com>
2022-08-21 11:38:32 +0400

    feat: dai tests (wip) ✨
    - Note that user1 denotes self, user2 = user2 and admin = user1

--- test/dai.ts
+++ test/dai.ts
@@ -7,6 +7,9 @@ const WAD = 10n ** 18n;
 const RAY = 10n ** 27n;
 const RAD = 10n ** 45n;
 
+const initialBalanceThis = l2Eth(1000n);
+const initialBalanceCal = l2Eth(100n);
+
 function sleep(ms: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
@@ -41,11 +44,11 @@ describe('dai', async function () {
     // Mint initial balances
     await admin.invoke(dai, 'mint', {
       account: _user1,
-      amount: { low: l2Eth(1000n).toDec()[0], high: l2Eth(1000n).toDec()[1] },
+      amount: { low: initialBalanceThis.toDec()[0], high: initialBalanceThis.toDec()[1] },
     });
     await admin.invoke(dai, 'mint', {
       account: _user2,
-      amount: { low: l2Eth(100n).toDec()[0], high: l2Eth(100n).toDec()[1] },
+      amount: { low: initialBalanceCal.toDec()[0], high: initialBalanceCal.toDec()[1] },
     });
 
     await starknet.devnet.dump('dump.pkl');
@@ -118,4 +121,149 @@ describe('dai', async function () {
       expect(err.message).to.contain('dai/insufficient-balance');
     }
   });
+
+  it('test approve sets allowance', async () => {
+    await user1.invoke(dai, 'approve', { spender: _user2, amount: l2Eth(25n).res });
+    expect(await dai.call('allowance', { owner: _user1, spender: _user2 })).to.deep.equal(
+      SplitUint.fromUint(25n)
+    );
+  });
+
+  it('charges amount approved', async () => {
+    const amountApproved = l2Eth(20n);
+    await user1.invoke(dai, 'approve', { spender: _user2, amount: amountApproved.res });
+    await user2.invoke(dai, 'transferFrom', {
+      sender: _user1,
+      recipient: _admin,
+      amount: amountApproved.res,
+    });
+    expect(await dai.call('balanceOf', { user: _admin })).to.deep.equal(amountApproved);
+  });
+
+  it('fail transfer without approval', async () => {
+    try {
+      await admin.invoke(dai, 'transferFrom', {
+        sender: _user1,
+        recipient: _user2,
+        amount: l2Eth(1n).res,
+      });
+    } catch (err: any) {
+      expect(err.message).to.contain('dai/insufficient-allowance');
+    }
+  });
+
+  it('fail charge more than approved', async () => {
+    const amountApproved = l2Eth(20n);
+    try {
+      await user1.invoke(dai, 'approve', { spender: _user2, amount: amountApproved.res });
+      await user2.invoke(dai, 'transferFrom', {
+        sender: _user1,
+        recipient: _admin,
+        amount: l2Eth(21n).res,
+      });
+    } catch (err: any) {
+      expect(err.message).to.contain('dai/insufficient-allowance');
+    }
+  });
+
+  it('test transfer from self', async () => {
+    await user1.invoke(dai, 'transferFrom', {
+      sender: _user1,
+      recipient: _admin,
+      amount: l2Eth(50n).res,
+    });
+    expect(await dai.call('balanceOf', { user: _admin })).to.deep.equal(l2Eth(50n));
+  });
+
+  it('test fail transfer from self non arbitrary size', async () => {
+    // you shouldn't be able to evade balance checks by transferring
+    // to yourself
+    const _balance: SplitUint = (await dai.call('balanceOf', { user: _admin })) as SplitUint;
+    try {
+      await user1.invoke(dai, 'transferFrom', {
+        sender: _user1,
+        recipient: _user1,
+        amount: l2Eth(1001n).res,
+      });
+    } catch (err: any) {
+      expect(err.message).to.contain('dai/insufficient-balance');
+    }
+  });
+
+  it('test mint self', async () => {
+    const mintAmount = l2Eth(10n);
+    await admin.invoke(dai, 'mint', { account: _admin, amount: mintAmount.res });
+    expect(await dai.call('balanceOf', { user: _admin })).to.deep.equal(mintAmount);
+  });
+
+  it('test mint guy', async () => {
+    const mintAmount = l2Eth(10n);
+    await admin.invoke(dai, 'mint', { account: _user1, amount: mintAmount.res });
+    expect(await dai.call('balanceOf', { user: _user1 })).to.deep.equal(
+      mintAmount.add(initialBalanceThis)
+    );
+  });
+
+  it('test fail mint guy no auth', async () => {
+    const mintAmount = l2Eth(10n);
+    try {
+      await user1.invoke(dai, 'mint', { account: _user1, amount: mintAmount.res });
+    } catch (err: any) {
+      expect(err.message).to.contain('dai/not-authorized');
+    }
+  });
+
+  it('test mint guy auth', async () => {
+    const mintAmount = l2Eth(10n);
+    await admin.invoke(dai, 'rely', { user: _user1 });
+    await user1.invoke(dai, 'mint', { account: _user1, amount: mintAmount.res });
+  });
+
+  it('test burn', async () => {
+    const burnAmount = l2Eth(10n);
+    await user1.invoke(dai, 'burn', { account: _user1, amount: burnAmount.res });
+    expect(await dai.call('totalSupply')).to.deep.equal(
+      initialBalanceThis.add(initialBalanceCal).sub(burnAmount)
+    );
+  });
+
+  it('test burn self', async () => {
+    const burnAmount = l2Eth(10n);
+    await user1.invoke(dai, 'burn', { account: _user1, amount: burnAmount.res });
+    expect(await dai.call('balanceOf', { user: _user1 })).to.deep.equal(
+      initialBalanceThis.sub(burnAmount)
+    );
+  });
+
+  it('test burn guy with trust', async () => {
+    const burnAmount = l2Eth(10n);
+    await user1.invoke(dai, 'approve', { spender: _admin, amount: burnAmount.res });
+    await admin.invoke(dai, 'burn', { account: _user1, amount: burnAmount.res });
+    expect(await dai.call('balanceOf', { user: _user1 })).to.deep.equal(
+      initialBalanceThis.sub(burnAmount)
+    );
+  });
+
+  it('test burn auth', async () => {
+    const burnAmount = l2Eth(10n);
+    await admin.invoke(dai, 'rely', { user: _user1 });
+    await user1.invoke(dai, 'burn', { account: _user1, amount: burnAmount.res });
+  });
+
+  it('test fail untrusted transfer from', async () => {
+    expect(await dai.call('allowance', { owner: _user1, spender: _user2 })).to.deep.equal(
+      l2Eth(0n)
+    );
+    try {
+      await admin.invoke(dai, 'transferFrom', {
+        sender: _user1,
+        recipient: _user2,
+        amount: l2Eth(200n).res,
+      });
+    } catch (err: any) {
+      expect(err.message).to.contain('dai/insufficient-allowance');
+    }
+  });
+
+  it('test trusting', async () => {});
 });
