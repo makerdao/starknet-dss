a2686283f5052b2deb11081969cf68b60198aab1
Evolve <Artevolve@yahoo.com>
2022-08-31 12:18:26 +0200

    fix: ci 🔨

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -64,7 +64,7 @@ const config: HardhatUserConfig = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   starknet: {
-    // venv: 'active',
+    venv: '/.venv',
     network: 'starknetLocal',
     recompile: true,
   },
