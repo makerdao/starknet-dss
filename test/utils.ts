8c341f6b1f5c8926de567ff0b585876b7374c9fa
Evolve <Artevolve@yahoo.com>
2022-09-06 11:56:25 +0200

    feat: helpers ✨
    - wad/rad/ray/uint helpers
    - invoke helper
    - move checkAuth in utils

--- test/utils.ts
+++ test/utils.ts
@@ -1,24 +1,31 @@
-import { BigNumber } from "ethers";
-import { parseEther } from "ethers/lib/utils";
-import { StarknetContract } from "hardhat/types";
-import fetch from "node-fetch";
+import { expect } from 'chai';
+import { BigNumber } from 'ethers';
+import { parseEther } from 'ethers/lib/utils';
+import { Account, StarknetContract } from 'hardhat/types';
+import fetch from 'node-fetch';
 
-type SplitUintType = { low: bigint; high: bigint };
+type SplitUintType<T> = { low: T; high: T };
 type numberish = string | number | bigint | BigNumber;
 
+const TEST_ADDRESS = '9379074284324409537785911406195';
+
+const WAD = 10n ** 18n;
+const RAY = 10n ** 27n;
+const RAD = 10n ** 45n;
+
 export function l2String(str: string): string {
-  return `0x${Buffer.from(str, "utf8").toString("hex")}`;
+  return `0x${Buffer.from(str, 'utf8').toString('hex')}`;
 }
 
 export class SplitUint {
-  res: SplitUintType;
+  res: SplitUintType<bigint>;
 
-  constructor(res: SplitUintType) {
+  constructor(res: SplitUintType<bigint>) {
     this.res = res;
   }
 
   static fromUint(a: numberish): SplitUint {
-    const bits = asHex(a).padStart(64, "0");
+    const bits = asHex(a).padStart(64, '0');
     const res = {
       low: BigInt(`0x${bits.slice(32)}`),
       high: BigInt(`0x${bits.slice(0, 32)}`),
@@ -37,7 +44,7 @@ export class SplitUint {
 
   add(_a: SplitUint | numberish): SplitUint {
     let a = _a as SplitUint;
-    if (!_a.hasOwnProperty("res")) {
+    if (!_a.hasOwnProperty('res')) {
       a = SplitUint.fromUint(_a as numberish);
     }
     return SplitUint.fromUint(this.toUint() + a.toUint());
@@ -45,7 +52,7 @@ export class SplitUint {
 
   sub(_a: SplitUint | numberish): SplitUint {
     let a = _a as SplitUint;
-    if (!_a.hasOwnProperty("res")) {
+    if (!_a.hasOwnProperty('res')) {
       a = SplitUint.fromUint(_a as numberish);
     }
     return SplitUint.fromUint(this.toUint() - a.toUint());
@@ -57,9 +64,7 @@ export class SplitUint {
 }
 
 function asHex(a: string | number | bigint | BigNumber): string {
-  return BigNumber.isBigNumber(a)
-    ? a.toHexString().slice(2)
-    : BigInt(a).toString(16);
+  return BigNumber.isBigNumber(a) ? a.toHexString().slice(2) : BigInt(a).toString(16);
 }
 
 export function split(a: BigNumber): bigint[] {
@@ -67,7 +72,7 @@ export function split(a: BigNumber): bigint[] {
 }
 
 export function toBytes32(a: string): string {
-  return `0x${BigInt(a).toString(16).padStart(64, "0")}`;
+  return `0x${BigInt(a).toString(16).padStart(64, '0')}`;
 }
 
 export function eth(amount: string) {
@@ -89,4 +94,64 @@ export async function simpleDeployL2(
 ): Promise<StarknetContract> {
   const factory = await hre.starknet.getContractFactory(name);
   return factory.deploy(args);
+}
+
+export async function invoke(
+  user: Account,
+  contract: StarknetContract,
+  selector: string,
+  data?: any
+) {
+  return user.invoke(contract, selector, data);
+}
+
+export async function checkAuth(base: any, contractName: string, admin: Account) {
+  const { res: ward } = await base.call('wards', { user: admin.starknetContract.address });
+
+  // await GodMode.setWard(base.address, this, 1);
+
+  expect((await base.call('wards', { user: TEST_ADDRESS })).res).to.equal(0n);
+
+  await invoke(admin, base, 'rely', { user: TEST_ADDRESS });
+
+  expect((await base.call('wards', { user: TEST_ADDRESS })).res).to.equal(1n);
+
+  await invoke(admin, base, 'deny', { user: TEST_ADDRESS });
+
+  expect((await base.call('wards', { user: TEST_ADDRESS })).res).to.equal(0n);
+
+  await invoke(admin, base, 'deny', { user: admin.starknetContract.address });
+
+  try {
+    await invoke(admin, base, 'rely', { user: TEST_ADDRESS });
+  } catch (err: any) {
+    expect(err.message).to.contain(`${contractName}/not-authorized`);
+  }
+  try {
+    await invoke(admin, base, 'deny', { user: TEST_ADDRESS });
+  } catch (err: any) {
+    expect(err.message).to.contain(`${contractName}/not-authorized`);
+  }
+
+  // await GodMode.setWard(base.address, this, ward);
+}
+
+export function wad(a: bigint): SplitUintType<string> {
+  const _a = SplitUint.fromUint(a * WAD);
+  return { low: _a.toDec()[0], high: _a.toDec()[1] };
+}
+
+export function ray(a: bigint): SplitUintType<string> {
+  const _a = SplitUint.fromUint(a * RAY);
+  return { low: _a.toDec()[0], high: _a.toDec()[1] };
+}
+
+export function rad(a: bigint): SplitUintType<string> {
+  const _a = SplitUint.fromUint(a * RAD);
+  return { low: _a.toDec()[0], high: _a.toDec()[1] };
+}
+
+export function uint(a: bigint): SplitUintType<string> {
+  const _a = SplitUint.fromUint(a);
+  return { low: _a.toDec()[0], high: _a.toDec()[1] };
 }
